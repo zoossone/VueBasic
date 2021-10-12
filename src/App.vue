fb3a681de35159cfd4abb59a30acbff2b535198a
zoossone <zoossone@gmail.com>
2021-10-12 23:23:10 +0900

    v-for

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,36 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="menu">
+    <!-- 반복문 사용법 v-for="XX in num" :ket="XX" -->
+    <!-- 숫자 부분에 데이터를 넣을 수 있음 -->
+    <a v-for="(a, i) in menus" :key="i">{{ a }}</a>
+  </div>
+
+  <div v-for="(a, i) in products" :key="i">
+    <h4>{{ a }}</h4>
+    <p>50 만원</p>
+  </div>
+  <!-- <div v-for="(a, i) in products" :key="i">
+    <h4>{{ products[i] }}</h4>
+    <p>50 만원</p>
+  </div> 
+  이런 형식도 가능-->
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  // 데이터 바인딩. data를 변경하면 data와 관련한 HTML에도 실시간으로 반영됨. {{}} 대신 이렇게 데이터 바인딩이 되어있어야 함.
+  // 고로 자주 변경되지 않는 데이터는 굳이 데이터바인딩을 하지 않아도 행복하다.
+  // HTML 속성도 데이터바인딩 가능.
+  // :속성="데이타이름"
+  data() {
+    return {
+      menus: ["Home", "Shop", "About"],
+      products: ["역삼동원룸", "천호동원룸", "마포구원룸"],
+    };
+  },
+  components: {},
+};
 </script>
 
 <style>
@@ -21,6 +40,16 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+.menu {
+  background: darkslateblue;
+  padding: 15px;
+  border-radius: 5px;
+}
+
+.menu a {
+  color: white;
+  padding: 10px;
 }
 </style>
