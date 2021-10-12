c0c45fd2dc69224ce2333de111fe5008d68ad469
zoossone <zoossone@gmail.com>
2021-10-12 23:53:00 +0900

    v-fro practicing

--- src/App.vue
+++ src/App.vue
@@ -5,15 +5,15 @@
     <a v-for="(a, i) in menus" :key="i">{{ a }}</a>
   </div>
 
-  <div>
-    <h4>{{ products[0] }}</h4>
-    <p>50 만원</p>
-    <button @click="increase(0)">허위매물신고</button>
-    <span>신고수: {{ reportNum[0] }}</span>
+  <div v-for="(a, i) in products" :key="i">
+    <h4>{{ a }}</h4>
+    <p>{{ price[i] }} 만원</p>
+    <button @click="increase(i)">허위매물신고</button>
+    <span>신고수: {{ reportNum[i] }}</span>
     <!-- v-on:   대신 @ 사용이 가능하다 
     mouseover 등의 다른 이벤트도 다양하다-->
   </div>
-  <div>
+  <!-- <div>
     <h4>{{ products[1] }}</h4>
     <p>60 만원</p>
     <button @click="increase(1)">허위매물신고</button>
@@ -24,7 +24,7 @@
     <p>70 만원</p>
     <button @click="increase(2)">허위매물신고</button>
     <span>신고수: {{ reportNum[2] }}</span>
-  </div>
+  </div> -->
 </template>
 
 <script>
@@ -34,9 +34,12 @@ export default {
   // 고로 자주 변경되지 않는 데이터는 굳이 데이터바인딩을 하지 않아도 행복하다.
   // HTML 속성도 데이터바인딩 가능.
   // :속성="데이타이름"
+
+  // Vue를 개발할떄는 데이터를 어떻게 할지 생각하며 만드는 것이 좋다.
   data() {
     return {
       reportNum: [0, 0, 0],
+      price: [50, 60, 70],
       menus: ["Home", "Shop", "About"],
       products: ["역삼동원룸", "천호동원룸", "마포구원룸"],
     };
